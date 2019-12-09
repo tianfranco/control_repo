9136b47cb256f67fb4cd3396d2bf5d76a35b3106
tianfranco <30939495+tianfranco@users.noreply.github.com>
2019-12-09 11:43:46 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file, 
-    content => 'this is a read me file - *',
-    owner => 'root',
-  }
-  
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
